03ef698648372c9d4a971472227fb6c8382c5299
𝐉𝐀𝐒𝐒 <150457649+jaskrnsroay@users.noreply.github.com>
2024-08-26 09:42:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "20617772"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "eb464a3d12219bbfbbbf2d4deb3b3151"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "6527727109"
+SUPPORT_GRP = "Dilwale_Haven"
+UPDATE_CHNL = "DugguWorld"
+OWNER_USERNAME = "imceobitxh"
 
